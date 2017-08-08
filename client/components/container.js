5513673638ea556c7f6e904057c675d67ad17ca8
Joel Guizar <joelguizar@gmail.com>
2017-08-08 16:42:34 -0700

    the rest

--- client/components/container.js
+++ client/components/container.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import List from './List/list.js';
+import PopularList from './Popular/PopularList.js';
 
 export default class App extends React.Component{
   render(){
     return (
-      <div style={{textAlign: 'center'}}>
-        <h1> What is gucci like sushi </h1>
+      <div className="container">
+        <h1> Container Component </h1>
+        <List />
+        <PopularList />
       </div>
     )
   }
