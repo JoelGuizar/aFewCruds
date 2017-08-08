17a8147e0600f5fe2b0f66229405bcedfdbde42e
Joel Guizar <joelguizar@gmail.com>
2017-08-08 15:09:15 -0700

    components

---
+++ client/components/container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class App extends React.Component{
+  render(){
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h1> What is gucci like sushi </h1>
+      </div>
+    )
+  }
+}
