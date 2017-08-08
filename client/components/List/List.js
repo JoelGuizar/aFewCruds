a27f38319d3d9767411b7897472326a3b725b657
Joel Guizar <joelguizar@gmail.com>
2017-08-08 16:41:59 -0700

    css react classes

---
+++ client/components/List/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Item from './Item.js';
+
+export default class List extends React.Component{
+  render(){
+    return (
+      <div className = "list">
+        <h1> List Component </h1>
+        <add
+        <Item />
+      </div>
+    )
+  }
+}
---
+++ client/components/Popular/PopularList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Recommendation from './Recommendation.js';
+
+export default class App extends React.Component{
+  render(){
+    return (
+      <div className='popList'>
+        <Recommendation />
+      </div>
+    )
+  }
+}
