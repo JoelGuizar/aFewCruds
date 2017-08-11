82fe337dcd2123060f6341bd4411a7cfb73eb60e
Joel Guizar <joelguizar@gmail.com>
2017-08-10 17:44:53 -0700

    server dependencies update json

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
-const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const app = express();
 
+const PORT = 9000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.get()
+
+app.listen(8000);
+
+app.get('')
