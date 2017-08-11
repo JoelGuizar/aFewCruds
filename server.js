38fc95406d3d3287b147cca7c29448b23df35259
Joel Guizar <joelguizar@gmail.com>
2017-08-10 18:00:07 -0700

    server fetch test

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const app = express();
 
 const PORT = 9000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 
-app.listen(8000);
+app.listen(8080);
 
-app.get('')
+app.post('/post', (res, req, error) => {
+  res.send("onSubmit handler sent");
+})
