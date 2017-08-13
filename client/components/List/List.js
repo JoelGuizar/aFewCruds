099f4dcd7f6a18710f5b07684e1fb4b0c81b0b28
Joel Guizar <joelguizar@gmail.com>
2017-08-12 18:20:22 -0700

    list

--- client/components/List/List.js
+++ client/components/List/List.js
@@ -2,16 +2,12 @@ import React from 'react';
 import Item from './Item.js';
 
 export default class List extends React.Component{
-  constructor(){
-    super();
-    this.state = {input: ""};
-    this.handleChange = this.handleChange.bind(this)
+  constructor(props){
+    super(props);
+    
   }
 
-  handleChange(e){
-    //e.preventDefault();
-    this.setState({input:e.target.value});
-  }
+
 
   render(){
     return (
@@ -27,7 +23,7 @@ export default class List extends React.Component{
         <form onSubmit= {this.props.addToList} >
           <label>
             Name:
-            <input type= 'text' value={this.state.input} onChange={this.handleChange} /> <br/>
+            <input type= 'text' value={this.props.yo} onChange={this.props.handleChange} /> <br/>
           </label>
             <input type= "submit" value= "Add" />
         </form>
--- client/components/container.js
+++ client/components/container.js
@@ -14,19 +14,17 @@ export default class App extends React.Component{
     this.handleList = this.handleList.bind(this);
     this.handlePopList = this.handlePopList.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleInput = this.handleInput.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleList(e){
     e.preventDefault();
     this.setState({'list': this.state.list.concat(this.state.input)});
     //console.log(e.target.value);
-    console.log(this.state.input);
+    this.setState({'input': ""});
   }
 
-  handleInput(item){
-    this.setState({input:input})
-  }
+
 
   handlePopList(e){
     e.preventDefault();
@@ -39,23 +37,27 @@ export default class App extends React.Component{
   handleSubmit(e){
     e.preventDefault();
     const self = this;
-    this.setState(value: "");
+    this.setState(input: "");
 
 
-    fetch('/users', {
+    fetch('/post', {
       method: 'POST',
-      data: this.state.value
+      data: this.state.input
     });
 
-    this.setState(value: "");
+    this.setState(input: "");
   }
 
+  handleChange(e){
+    //e.preventDefault();
+    this.setState({input:e.target.value});
+  }
 
   render(){
     return (
       <div className="container">
         <h1> Container Component </h1>
-        <List list= {this.state.list} addToList= {this.handleList}/>
+        <List list= {this.state.list} addToList= {this.handleList} handleChange= {this.handleChange} yo={this.state.input}/>
         <PopularList popList= {this.state.popList} addToPopList= {this.handlePopList}/>
       </div>
     )
