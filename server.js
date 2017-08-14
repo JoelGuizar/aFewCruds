5aed473fa9bf0ecea2817c4f6cae00fd3db795a0
Joel Guizar <joelguizar@gmail.com>
2017-08-13 20:39:24 -0700

    nodemon second server

--- server.js
+++ server.js
@@ -4,15 +4,22 @@ const cookieParser = require('cookie-parser');
 // const bodyParser = require('body-parser');
 const app = express();
 
-const PORT = 8080;
+const PORT = 8888;
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 
-app.listen(8080);
+app.listen(8080, function(){
+  console.log("listening on ", PORT);
+});
 
-app.get('/yo', (res, req, error) => {
+app.get('/', (req, res) => {
+  res.send("./client/index.html");
+  res.end();
+})
+
+app.get('/yo', (req, res) => {
   console.log("LOL");
   res.send("onSubmit handler sent");
 })
